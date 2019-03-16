137c53c30e9b960758c26ffa62ede61130e3c953
WitDarek <witdarek88@gmail.com>
2019-03-17 00:59:01 +0100

    added image and metode changing polish characters.

--- src/main/java/pl/sda/javafx/controller/RootController.java
+++ src/main/java/pl/sda/javafx/controller/RootController.java
@@ -5,14 +5,11 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import pl.sda.javafx.serivce.WeatherService;
-
-
-import javax.swing.text.html.ImageView;
-import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
-
 import java.util.ResourceBundle;
 
 public class RootController implements Initializable {
@@ -21,8 +18,7 @@ public class RootController implements Initializable {
     private Button search;
     @FXML
     private TextField city;
-   /* @FXML
-    private Label Text1;*/
+
     @FXML
     private Label TemperatureCIn;
     @FXML
@@ -65,22 +61,88 @@ public class RootController implements Initializable {
     }
     public void setCity() throws IOException {
 
-        WeatherService weatherService =new WeatherService ("http://api.apixu.com/v1/current.json","1b301cfd93fe4ee0bd9123325191003");
-       TemperatureCIn.setText((weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getTemp_c ())+" °C");
-       CityIn.setText (weatherService.getCityWeather (city.getText ()).getLocation ().getName ());
-       CountryIn.setText (weatherService.getCityWeather (city.getText ()).getLocation ().getCountry ());
-       LocalTime.setText (weatherService.getCityWeather (city.getText ()).getLocation ().getLocaltime ());
-       UpdateTime.setText (weatherService.getCityWeather (city.getText ()).getCurrent ().getLast_updated ());
-       Description.setText (weatherService.getCityWeather (city.getText ()).getCurrent ().getCondition ().getText ());
-       FeelTemperatureCIn.setText (weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getFeelslike_c ()+ " °C");
-       FeelTemperatureFIn.setText (weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getFeelslike_f ()+ " °F");
-       TemperatureFIn.setText (weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getTemp_f ()+ " °F");
-       WindKphIn.setText (weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getWind_kph ()+ " kph");
-       WindMphIn.setText (weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getWind_mph ()+ " mph");
-       PressureIn.setText (weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getPressure_mb ()+ "hPa");
-       HumdityIn.setText (weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getHumidity ()+ "%");
-       Cloudy.setText (weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getCloud ()+ "%");
-       //WeatherImage.getImageURL (new URL (weatherService.getCityWeather (city.getText ()).getCurrent ().getCondition ().getIcon ()));
+    WeatherService weatherService = new WeatherService ("http://api.apixu.com/v1/current.json", "1b301cfd93fe4ee0bd9123325191003");
+    city.setText (ChangePolishChars (city.getText ()));
+    TemperatureCIn.setText ((weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getTemp_c ()) + " °C");
+    CityIn.setText (weatherService.getCityWeather (city.getText ()).getLocation ().getName ());
+    CountryIn.setText (weatherService.getCityWeather (city.getText ()).getLocation ().getCountry ());
+    LocalTime.setText (weatherService.getCityWeather (city.getText ()).getLocation ().getLocaltime ());
+    UpdateTime.setText (weatherService.getCityWeather (city.getText ()).getCurrent ().getLast_updated ());
+    Description.setText (weatherService.getCityWeather (city.getText ()).getCurrent ().getCondition ().getText ());
+    FeelTemperatureCIn.setText (weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getFeelslike_c () + " °C");
+    FeelTemperatureFIn.setText (weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getFeelslike_f () + " °F");
+    TemperatureFIn.setText (weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getTemp_f () + " °F");
+    WindKphIn.setText (weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getWind_kph () + " kph");
+    WindMphIn.setText (weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getWind_mph () + " mph");
+    PressureIn.setText (weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getPressure_mb () + " hPa");
+    HumdityIn.setText (weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getHumidity () + "%");
+    Cloudy.setText (weatherService.getCityWeather (String.valueOf (city.getText ())).getCurrent ().getCloud () + "%");
+    WeatherImage.setImage (new Image ("http:" + weatherService.getCityWeather (city.getText ()).getCurrent ().getCondition ().getIcon ()));
+}
 
+
+
+    public String ChangePolishChars(String city){
+
+        for (int i = 0; i < city.length (); i++)
+            switch (city.charAt (i)) {
+
+                case 'ą':
+                    city = city.replace ('ą', 'a');
+                    break;
+                case 'ć':
+                    city = city.replace ('ć', 'c');
+                    break;
+                case 'ę':
+                    city = city.replace ('ę', 'e');
+                    break;
+                case 'ł':
+                    city = city.replace ('ł', 'l');
+                    break;
+                case 'ń':
+                    city = city.replace ('ń', 'n');
+                    break;
+                case 'ó':
+                    city = city.replace ('ó', 'o');
+                    break;
+                case 'ś':
+                    city = city.replace ('ś', 's');
+                    break;
+                case 'ź':
+                    city = city.replace ('ź', 'z');
+                    break;
+                case 'ż':
+                    city = city.replace ('ż', 'z');
+                    break;
+                case 'Ą':
+                    city = city.replace ('Ą', 'A');
+                    break;
+                case 'Ć':
+                    city = city.replace ('Ć', 'c');
+                    break;
+                case 'Ę':
+                    city = city.replace ('Ę', 'E');
+                    break;
+                case 'Ł':
+                    city = city.replace ('Ł', 'L');
+                    break;
+                case 'Ń':
+                    city = city.replace ('Ń', 'N');
+                    break;
+                case 'Ó':
+                    city = city.replace ('Ó', 'O');
+                    break;
+                case 'Ś':
+                    city = city.replace ('Ś', 'S');
+                    break;
+                case 'Ź':
+                    city = city.replace ('Ź', 'Z');
+                    break;
+                case 'Ż':
+                    city = city.replace ('Ż', 'Z');
+                    break;
+
+            }
+       return city;
     }
 }
